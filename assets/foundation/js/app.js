16c0beac071605374856ad73e7538939460bc04f
Oscar Kali <okali@CIM.CO.KE>
2017-11-16 20:43:15 +0300

    part 2

--- assets/foundation/js/app.js
+++ assets/foundation/js/app.js
@@ -1,71 +1,135 @@
+import { multiply } from './export';
+
+
 const prog = [1, 2, 3];
 let digits = new Array();
 
-function ite() {
-    arg = Array.from(arguments);
-    return arg.reduce((prev, curr) => prev + curr);
-}
+class Buildname {
 
-let play = (name) => {
-        // let c = document.getElementById("varE");
-        // var first = document.getElementById("varB");
-        // var second = document.getElementById("varC");
-        // var third = document.getElementById("varD");
-        // var name = document.getElementById("varA");
-
-        console.log(digits);
-
-        let [q, w, e] = digits;
-
-        // const prog = 098;
-        // console.log(prog);
-
-        //call the shorthand
-        console.log(getname(name, "098"));
-
-        let result = getname(name, "098");
-
-        //convert an object to array
-        var arr = Array.from(Object.keys(result), k => result[k]);
-
-        // distructing
-        let [x, y, z] = arr;
-
-        //spread arrays in 
-        let one = [q, w, e, ...prog];
-
-        // distruction
-        let [a, b, ...theRest] = one;
-
-        console.log(ite(a, b, ...theRest));
-        if (ite(a, b, ...theRest) <= 15) {
-            alert(`${x} you became a winner at ${z} , you gave values that add upto ${ite(a, b, ...theRest)} `);
-        } else {
-            alert(x + 'at' + z + ' , you gave values that add upto' + ite(a, b, ...theRest));
-        }
-
+    constructor(name) {
+        this.name = name;
+        this.number = 'Not Available';
     }
-    // Object shorthand and method shorthand using fat arrow
-let getname = (name, number) => {
-    return {
-        name,
-        number,
-        timestamp: Date.now()
+    get pnumber() {
+        return this.number;
+    }
+
+    set pnumber(numb) {
+        this.number = numb;
+    }
+    pushname() {
+        return play(this.name);
     }
 }
 
+class Buildnameandamount extends Buildname {
+    constructor(name, amount) {
+        super(name);
+        this.amount = amount;
+    }
+    callplay() {
+        return play(this.name, this.amount);
+    }
+}
+
+class CustomDate extends Date {
+    constructor() {
+        super();
+    }
+    getFormattedDate() {
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+            'Oct', 'Nov', 'Dec'
+        ];
+        return this.getDate() + '-' + months[this.getMonth()] + '-' + this.getFullYear();
+    }
+    static today() {
+        return `today`;
+    }
+}
+
+
+// logic function
+let play = (name, amount) => {
+
+    let result = new Buildnameandamount(name, amount);
+
+    //call to the set and get
+    result.pnumber = 071234567;
+    // convert an object to array
+    var arr = Array.from(Object.keys(result), k => result[k]);
+
+    // distructing
+    let [x, y, z] = arr;
+
+    console.log(`${arr} this is an array fro object`);
+
+    function getAmount(cashamount) {
+        var isAnyNegative = () => {
+                // avoid negative values
+                return cashamount < 0;
+            },
+            varisbelowmin = () => {
+                //no gambling on amount less than 10
+                return cashamount < 10;
+            }
+        var promise = new Promise(function(resolve, reject) {
+            if (isAnyNegative()) {
+                reject("Your account is below zero");
+            } else if (varisbelowmin()) {
+                reject("You don't have enough money");
+            }
+            resolve(cashamount);
+        });
+        return promise;
+    }
+    getAmount(amount)
+        .then(function(amount) {
+            function* getluckynumber(value) {
+                    let index = 0;
+                    while (index <= 1) {
+                        yield Math.floor(Math.random() * 10 + 1);
+                    }
+                }
+                // use generator
+            let gen = getluckynumber(amount);
+            let generator1 = gen.next();
+            //use impoter
+            let winno = multiply(generator1.value, 1);
+
+            const usersdigits = new Set(digits);
+            console.log(usersdigits);
+            console.log(winno);
+            // console.log(winno.value);
+
+
+            if (usersdigits.has(winno.value)) {
+                alert(`Congratulations ${x} your lucky number is ${winno.value} you have won ${amount * 2}, ${CustomDate.today}`);
+            } else {
+                alert(`Sorry ${x} todays lucky number is ${winno.value} you have lost ${amount}`);
+            }
+
+        })
+        .catch(function(e) {
+            console.log(e);
+            alert(e);
+        });
+
+}
 var getelements = () => {
         // var first = 1;
         // var second = 2;
         // var third = 3;
         // var name = 'oscar';
+        // var amount = 100;
         var first = +document.getElementById('first').value;
         var second = +document.getElementById("second").value;
         var third = +document.getElementById("third").value;
         var name = document.getElementById("name").value;
+        var amount = document.getElementById("amount").value;
 
         digits.push(first, second, third);
 
-        play(name);
+        let start = new Buildnameandamount(name, amount);
+        start.callplay();
     }
     // getelements();
