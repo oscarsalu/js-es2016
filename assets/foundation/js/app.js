f9716fcf9380c218cd39c03b9f20ba027723a56b
Oscar Kali <okali@CIM.CO.KE>
2017-11-16 08:38:26 +0300

    array push

--- assets/foundation/js/app.js
+++ assets/foundation/js/app.js
@@ -1,20 +1,24 @@
 const prog = [1, 2, 3];
+let digits = new Array();
 
 function ite() {
     arg = Array.from(arguments);
     return arg.reduce((prev, curr) => prev + curr);
 }
 
-let play = () => {
+let play = (name) => {
         // let c = document.getElementById("varE");
-        var first = document.getElementById("varB");
-        var second = document.getElementById("varC");
-        var third = document.getElementById("varD");
-        var name = document.getElementById("varA");
+        // var first = document.getElementById("varB");
+        // var second = document.getElementById("varC");
+        // var third = document.getElementById("varD");
+        // var name = document.getElementById("varA");
 
+        console.log(digits);
 
-        const prog = 098;
-        console.log(prog);
+        let [q, w, e] = digits;
+
+        // const prog = 098;
+        // console.log(prog);
 
         //call the shorthand
         console.log(getname(name, "098"));
@@ -28,24 +32,40 @@ let play = () => {
         let [x, y, z] = arr;
 
         //spread arrays in 
-        let one = [first, second, third, ...prog];
+        let one = [q, w, e, ...prog];
 
         // distruction
         let [a, b, ...theRest] = one;
 
         console.log(ite(a, b, ...theRest));
         if (ite(a, b, ...theRest) <= 15) {
-            alert(`${x} you became a winner at ${z} , your gave values that add upto ${ite(a, b, ...theRest)} `);
+            alert(`${x} you became a winner at ${z} , you gave values that add upto ${ite(a, b, ...theRest)} `);
+        } else {
+            alert(x + 'at' + z + ' , you gave values that add upto' + ite(a, b, ...theRest));
         }
 
-        alert(`${x} at ${z} , your gave values that add upto ${ite(a, b, ...theRest)} `);
     }
     // Object shorthand and method shorthand using fat arrow
 let getname = (name, number) => {
-        return {
-            name,
-            number,
-            timestamp: Date.now()
-        }
+    return {
+        name,
+        number,
+        timestamp: Date.now()
     }
-    //play();
+}
+
+var getelements = () => {
+        // var first = 1;
+        // var second = 2;
+        // var third = 3;
+        // var name = 'oscar';
+        var first = +document.getElementById('first').value;
+        var second = +document.getElementById("second").value;
+        var third = +document.getElementById("third").value;
+        var name = document.getElementById("name").value;
+
+        digits.push(first, second, third);
+
+        play(name);
+    }
+    // getelements();
